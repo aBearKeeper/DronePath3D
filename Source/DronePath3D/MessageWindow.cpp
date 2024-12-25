7111a1fc9c77c7220e140333f0bac0ba5b790c2a
162130326 <799417468@qq.com>
2024-12-25 16:19:31 +0800

    实现了简易消息框

--- Source/DronePath3D/MessageWindow.cpp
+++ Source/DronePath3D/MessageWindow.cpp
@@ -1,5 +1,31 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MessageWindow.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
 
+void UMessageWindow::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    // 绑定按钮点击事件
+    if (ConfirmButton)
+    {
+        ConfirmButton->OnClicked.AddDynamic(this, &UMessageWindow::OnConfirmButtonClicked);
+    }
+}
+
+void UMessageWindow::OnConfirmButtonClicked()
+{
+    // 点击确认后移除弹窗
+    RemoveFromParent();
+}
+
+void UMessageWindow::SetMessage(const FString& NewMessage)
+{
+    Message = NewMessage;
+
+    // 设置TextBlock显示错误信息
+    if (MessageText)
+    {
+        MessageText->SetText(FText::FromString(Message));
+    }
+}
--- Source/DronePath3D/MessageWindow.h
+++ Source/DronePath3D/MessageWindow.h
@@ -1,17 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
 #include "MessageWindow.generated.h"
 
 /**
- * 
+ * 自定义错误消息弹窗类
  */
 UCLASS()
 class DRONEPATH3D_API UMessageWindow : public UUserWidget
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    // 初始化构造
+    virtual void NativeConstruct() override;
+
+    // 确认按钮点击事件
+    UFUNCTION()
+    void OnConfirmButtonClicked();
+
+    // 设置错误消息
+    UFUNCTION(BlueprintCallable, Category = "MessageWindow")
+    void SetMessage(const FString& Message);
+
+    // 绑定控件
+    UPROPERTY(meta = (BindWidget))
+    UButton* ConfirmButton;  // 确认按钮
+
+    UPROPERTY(meta = (BindWidget))
+    UTextBlock* MessageText;  // 显示信息的TextBlock
+
+private:
+    // 存储信息
+    FString Message;
 };
