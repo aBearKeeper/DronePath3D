94e499b768a5adfea8325b667e91ac4c9e2a07e6
162130326 <799417468@qq.com>
2024-12-25 14:31:10 +0800

    "实现了简易消息弹窗"

---
+++ Source/DronePath3D/MessageWindow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MessageWindow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONEPATH3D_API UMessageWindow : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
