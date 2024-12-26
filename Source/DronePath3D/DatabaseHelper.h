59a204597f2c75d887fafc93f6de72145562efe5
Xie Zhoufeng <767873532@qq.com>
2024-12-26 20:16:29 +0800

    数据库辅助器对蓝图不再可见

--- Source/DronePath3D/DatabaseHelper.h
+++ Source/DronePath3D/DatabaseHelper.h
@@ -26,43 +26,30 @@ class DRONEPATH3D_API UDatabaseHelper : public UObject
 
 public:
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	static UDatabaseHelper* GetSingleton();
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool execute(FString sql);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool AddNewScene(FString FilePath);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool DeleteScene(int32 SceneID);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	TArray<USceneInfo*> GetAllScenes();
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool AddNewDrone(const UDroneInfo* DroneInfo);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool DeleteDrone(int32 DroneID);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool UpdateDroneInfo(UDroneInfo* DroneInfo);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	TArray<UDroneInfo*> GetAllDrones();
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool AddNewTargetPoint(const UTargetPointInfo* TargetPointInfo);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool DeleteTargetPoint(int32 TargetPointID);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	bool UpdateTargetPointInfo(UTargetPointInfo* TargetPointInfo);
 
-	UFUNCTION(BlueprintCallable, Category = "DatabaseHelper")
 	TArray<UTargetPointInfo*> GetAllTargetPoints();
 
 private:
